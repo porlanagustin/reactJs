86eddda23d23e765ba5340955ab35ad12c75a99f
porlanagustin <porlan.agustin@gmail.com>
2023-06-08 20:47:36 -0300

    react modify

---
+++ myapp/src/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return <header>
+        <ul>
+            <li>inicio</li>
+            <li>about</li>
+            <li>contacto</li>
+        </ul>
+    </header>
+}
+
+export default Navbar;
